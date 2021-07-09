4cacf7108284b8e8f0f17829df6d7b65dc9dcf8c
danielbossier <dbossier21@gmail.com>
2021-07-08 22:19:19 -0700

    added searchable action for teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,6 @@
 class TeamsController < ApplicationController
+  def index
+    response = HTTP.get("https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=#{params[:search]}")
+    render json: response.parse(:json)
+  end
 end
